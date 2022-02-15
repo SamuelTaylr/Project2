03d69e7b430685a3099caf33ad0a99d3139436f1
HamingnottT <Jnottingham836@yahoo.com>
2022-02-15 14:16:27 -0600

    Setup Commit

---
+++ src/main/scala/jake.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.sql.SparkSession
+
+class jake {
+  def dataLoader(spark: SparkSession): Unit = {
+
+    import spark.implicits._
+
+    val preDf = spark.sparkContext.textFile("input/covid_19_data.csv")
+    val df = preDf.map(_.split(",")).map{case Array(a,b,c,d,e,f,g,h) => (a,b,c,d,e,f,g,h)}.toDF("Id",
+      "Obs_Date","State","Country","Update","Confirmed","Deaths","Recovered")
+
+    df.show(10)
+
+
+
+  }
+}
