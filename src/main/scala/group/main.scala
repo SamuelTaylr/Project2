b9996b7255fcc2f5ebe7cd96d0225749e97de4f8
mark-coffer <98058198+mark-coffer@users.noreply.github.com>
2022-02-17 05:23:01 -0500

    login verification function established and monthly avg query done

--- src/main/scala/group/main.scala
+++ src/main/scala/group/main.scala
@@ -1,9 +1,11 @@
+package group
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
 
 object main {
 
-  System.setProperty("hadoop.home.dir", "C:\\hadoop\\")
+  System.setProperty("hadoop.home.dir", "C:\\winutils")
 
   val spark = SparkSession
     .builder
@@ -16,15 +18,23 @@ object main {
   import spark.implicits._
   spark.sparkContext.setLogLevel("ERROR")
 
-
-
   def main(args: Array[String]): Unit = {
 
-    val sam = new sam
-    sam.dataLoader(spark)
-    println("Hello World")
-    println("This is a change ")
+    /*val sam = new sam
+    sam.dataLoader(spark)*/
 
+    val covidDf = mark.dataLoader(spark)
+    //mark.sparkQuery1(covidDf, spark)
 
+    /* --I created the following to test my attempt at login verification
+    covidDf.createOrReplaceTempView("toTest")
+
+    val testing = spark.sql("select case when count(country) >= 1 then true else false end as toBool from toTest where country = 'US'")
+    val boolTest = testing.first().get(0)
+    val finalBool = boolTest.asInstanceOf[Boolean]
+    println(finalBool)
+    if(finalBool) {println("this works")} else {println("dammit")}*/
+
+    mark.avgCasesByMonth(covidDf, spark)
   }
 }
--- src/main/scala/group/mark.scala
+++ src/main/scala/group/mark.scala
@@ -2,6 +2,7 @@ package group
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions.to_date
+import scala.io.StdIn
 
 object mark {
 
@@ -23,6 +24,48 @@ object mark {
     inputDF.createOrReplaceTempView("query1")
     val result = spark.sql("SELECT Obs_Date, SUM(Confirmed) FROM query1 GROUP BY Obs_Date ")
     result.show()
+  }
+  /*given table name and 2 columns from that table,
+      receive 2 values via user input to query in that table
+      return true if the query succeeds, else false --mac
+      code is currently commented out but can be re-implemented if desired
+   */
+  def userLogin(spark : SparkSession/*, table : String, Param1 : String, Param2 : String*/): Boolean = {
+    val username = StdIn.readLine("Please enter your username: ").toUpperCase()
+    val password = StdIn.readLine("Please enter your password").toUpperCase()
+
+    val verifyLogin = spark.sql("select" +
+      s"\n\tcase when count(username) >= 1 and count(password) >= 1 then" +
+        "\n\t\ttrue" +
+      "\n\telse" +
+        "\n\t\tfalse" +
+      "\n\tend as boolAnswer" +
+      s"\nfrom login" +
+        s"\n\twhere upper(username)=upper($username) and upper(password)=upper($password);")
+
+    val verifyResult = verifyLogin.first().get(0)
+    verifyResult.asInstanceOf[Boolean]
+  }
+
+  def avgCasesByMonth(inputDF : DataFrame, spark : SparkSession): Unit = {
+    inputDF.createOrReplaceTempView("AvgQuery")
+    spark.sql("(select round(avg(Confirmed), 2) from AvgQuery As Jan2020_Avg where Obs_Date Between '2020-01-22' and '2020-01-31') " +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Feb2020_Avg where Obs_Date Between '2020-02-01' and '2020-02-29')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Mar2020_Avg where Obs_Date Between '2020-03-01' and '2020-03-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Apr2020_Avg where Obs_Date Between '2020-04-01' and '2020-04-30')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As May2020_Avg where Obs_Date Between '2020-05-01' and '2020-05-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Jun2020_Avg where Obs_Date Between '2020-06-01' and '2020-06-30')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Jul2020_Avg where Obs_Date Between '2020-07-01' and '2020-07-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Aug2020_Avg where Obs_Date Between '2020-08-01' and '2020-08-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Sep2020_Avg where Obs_Date Between '2020-09-01' and '2020-09-30')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Oct2020_Avg where Obs_Date Between '2020-10-01' and '2020-10-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Nov2020_Avg where Obs_Date Between '2020-11-01' and '2020-11-30')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Dec2020_Avg where Obs_Date Between '2020-12-01' and '2020-12-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Jan2021_Avg where Obs_Date Between '2021-01-01' and '2021-01-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Feb2021_Avg where Obs_Date Between '2021-02-01' and '2021-02-28')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Mar2021_Avg where Obs_Date Between '2021-03-01' and '2021-03-31')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As Apr2021_Avg where Obs_Date Between '2021-04-01' and '2021-04-30')" +
+      "UNION ALL(select round(avg(Confirmed), 2) from AvgQuery As May2021_Avg where Obs_Date Between '2021-05-01' and '2021-29-05')").show()
   }
 
 }
